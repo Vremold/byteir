3b86648a6e1ca028237f1ab014f9070647419ecd
Chengji Yao <chengji.yao@bytedance.com>
2021-12-04 03:25:41 +0000

    [CMAKE] fix bug when byteir build embedded

--- cmake/mhlo.cmake
+++ cmake/mhlo.cmake
@@ -1,8 +1,7 @@
 set(MHLO_BUILD_EMBEDDED ON)
-add_subdirectory(${REPO_ROOT_DIR}/external/mlir-hlo ${CMAKE_CURRENT_BINARY_DIR}/mlir-hlo)
+add_subdirectory(${REPO_ROOT_DIR}/external/mlir-hlo ${CMAKE_CURRENT_BINARY_DIR}/mlir-hlo EXCLUDE_FROM_ALL)
 include_directories(${REPO_ROOT_DIR}/external/mlir-hlo/include)
-include_directories(${CMAKE_BINARY_DIR}/mlir-hlo/include)
-include_directories(${CMAKE_BINARY_DIR})
+include_directories(${CMAKE_CURRENT_BINARY_DIR}/mlir-hlo/include)
 
 install(DIRECTORY ${REPO_ROOT_DIR}/external/mlir-hlo/include/mlir-hlo ${REPO_ROOT_DIR}/external/mlir-hlo/include/mlir-hlo-c
   DESTINATION external/include
