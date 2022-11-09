ea5e50fa4252d837513496211b7b05e48b7fd267
刘渊强 <liuyuanqiang.yqliu@bytedance.com>
2022-11-09 07:02:19 +0000

    remove python bindings

--- python/byteir/ir/__init__.py
+++
@@ -1,4 +0,0 @@
-from .executor import IRExecutor, MhloCustomCallExecutor
-from .helper import mlir_type_to_dtype, mlir_attr_to_pyobj
-from .backend import register_all_backends
-
--- python/byteir/ir/backend/__init__.py
+++
@@ -1,3 +0,0 @@
-def register_all_backends():
-    from .mhlo import _dispatch_mhlo
-    from .tf import _dispatch_tf
