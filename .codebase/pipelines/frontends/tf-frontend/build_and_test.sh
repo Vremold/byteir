bcdd3668cfe8190aea61ed57ee818b4752cff237
Chengji Yao <chengji.yao@bytedance.com>
2023-03-11 01:06:08 +0000

    [TF-Frontend] simplify build scripts
    * simplify build scripts
    * refine scripts
    
    https://code.byted.org/byteir/byteir/merge_requests/445

--- .codebase/pipelines/frontends/tf-frontend/build_and_test.sh
+++ .codebase/pipelines/frontends/tf-frontend/build_and_test.sh
@@ -12,27 +12,7 @@ export http_proxy='http://sys-proxy-rd-relay.byted.org:8118';
 export https_proxy='http://sys-proxy-rd-relay.byted.org:8118';
 export no_proxy='*.byted.org'
 
-git submodule update --init --recursive $PROJ_DIR/external/tensorflow
-git submodule update -f $PROJ_DIR/external/tensorflow
-
-# configure bazel
-BAZEL_VERSION=$(cat $PROJ_DIR/external/tensorflow/.bazelversion)
-if [ ! -f "bazel-$BAZEL_VERSION-linux-x86_64" ]; then
-  wget https://github.com/bazelbuild/bazel/releases/download/$BAZEL_VERSION/bazel-$BAZEL_VERSION-linux-x86_64 -q
-fi
-cp bazel-$BAZEL_VERSION-linux-x86_64 $PROJ_DIR/bazel -f
-chmod +x $PROJ_DIR/bazel
-cp $PROJ_DIR/.tf_configure.bazelrc $PROJ_DIR/external/tensorflow/.tf_configure.bazelrc
-
-# apply patches
-pushd $PROJ_DIR/external/tensorflow
-git apply ../patches/tensorflow/tf_build.patch
-git apply ../patches/tensorflow/arm_codegen.patch
-git apply ../patches/tensorflow/tf_dilated_conv.patch
-git apply ../patches/tensorflow/tf_slice.patch
-git apply ../patches/tensorflow/mhlo_ops.patch
-git apply ../patches/tensorflow/grappler.patch
-popd
+bash $PROJ_DIR/scripts/prepare.sh
 
 # build and test
 pushd $PROJ_DIR
