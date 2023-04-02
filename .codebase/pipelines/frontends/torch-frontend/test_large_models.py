8094334d90c7b3b7c5c57898977d7bb0b3674eed
Yuanqiang Liu <liuyuanqiang.yqliu@bytedance.com>
2023-04-02 21:19:23 +0800

    [torch-frontend] add numerical test for real models

--- .codebase/pipelines/frontends/torch-frontend/test_large_models.py
+++ .codebase/pipelines/frontends/torch-frontend/test_large_models.py
@@ -1,5 +1,6 @@
 import torch
 from functorch import make_fx
+import numpy as np
 
 import os
 import tempfile
@@ -7,6 +8,9 @@ from pathlib import Path
 
 import torch_frontend
 from torch_frontend import convert_to_mhlo_via_torch_mlir
+
+from mhlo_tools.ir_executor import Interpreter
+from mhlo_tools.ir_executor.helper import mlir_attr_to_pyobj
 
 LARGE_MODEL_PATH = os.environ["TORCH_LARGE_MODEL_PATH"]
 MODEL_LIST = ["sar_relevance_cross_model_latest/28365.ts", "tt_label3_0607/torch_model_1654572315533.jit.revert.ts"]
@@ -43,7 +47,8 @@ def convert_to_mhlo_via_torch_mlir(jit_model_path):
     dir = os.path.abspath(os.path.dirname(jit_model_path))
     ts_module = torch.jit.load(jit_model_path, map_location="cuda").eval()
     sample_inputs = torch.load(os.path.join(dir, "batch_sample_inputs"), map_location="cuda")
-    print(ts_module(*sample_inputs))
+    torch_outputs = ts_module(*sample_inputs)
+    print(torch_outputs)
 
     torch_frontend.register_decomposition_in_torchscript()
     torch._C._jit_pass_inline(ts_module.graph)
@@ -54,11 +59,24 @@ def convert_to_mhlo_via_torch_mlir(jit_model_path):
     ts_module = reload_model(ts_module).eval()
 
     module = torch_frontend.convert_to_mhlo_via_torch_mlir(ts_module, sample_inputs)
-    print(module.operation.get_asm(large_elements_limit=10, enable_debug_info=False))
+    mhlo_file = "/tmp/tmp.mhlo.mlir"
+    with open(mhlo_file, "w") as f:
+        print(module.operation.get_asm(enable_debug_info=False), file=f)
+
+    # run mhlo-tools and compare outputs' diff
+    with Interpreter.load_from_file(mhlo_file) as interp:
+        mhlo_outputs = interp.call_function("forward", [i.detach().cpu().numpy() for i in sample_inputs])
+        print(mhlo_outputs)
+        if isinstance(torch_outputs, torch.Tensor):
+            torch_outputs = [torch_outputs.detach().cpu().numpy()]
+        else:
+            torch_outputs = [i.detach().cpu().numpy() for i in torch_outputs]
+        np.testing.assert_almost_equal(mhlo_outputs, torch_outputs, decimal=2)
 
 
 def test_large_models():
     for model in MODEL_LIST:
+        print(model)
         convert_to_mhlo_via_torch_mlir(os.path.join(LARGE_MODEL_PATH, model))
 
 if __name__ == "__main__":
