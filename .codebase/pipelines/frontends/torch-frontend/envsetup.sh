7c45aee71d397b0617c6d70935ff51c99002c03a
刘渊强 <liuyuanqiang.yqliu@bytedance.com>
2023-04-20 03:02:44 +0000

    [torch-frontend] update torch mlir
    * [torch-frontend] update torch-mlir
    * update
    
    https://code.byted.org/byteir/byteir/merge_requests/526

--- .codebase/pipelines/frontends/torch-frontend/envsetup.sh
+++ .codebase/pipelines/frontends/torch-frontend/envsetup.sh
@@ -13,7 +13,7 @@ TORCH_MLIR_ROOT="$PROJ_DIR/third_party/torch-mlir"
 function download_llvm_prebuilt() {
   pushd ${PROJ_DIR}
   if [[ -z ${LLVM_INSTALL_DIR} ]]; then
-    LLVM_BUILD="llvm_install_225d255a583ea3d50bbba49d949ca76be6a880bf.tar.gz"
+    LLVM_BUILD="llvm_install_3abae1c88416858cf2e9a7ed9417bc52033933b4.tar.gz"
     if [ ! -f "$LLVM_BUILD" ]; then
       rm -rf llvm_install*
       rm -rf llvm_build
