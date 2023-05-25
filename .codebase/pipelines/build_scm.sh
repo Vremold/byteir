4a72facbbd3291650f9d8bc76919c45514a4be01
Yuanqiang Liu <liuyuanqiang.yqliu@bytedance.com>
2023-05-25 15:52:22 +0800

    [scm] release torch-frontend on scm

--- .codebase/pipelines/build_scm.sh
+++ .codebase/pipelines/build_scm.sh
@@ -11,12 +11,17 @@ OUTPUT_DIR="$ROOT_PROJ_DIR/output"
 pushd $ROOT_PROJ_DIR
 
 export CUDACXX=/usr/local/cuda/bin/nvcc
+# build byteir
 bash $ROOT_PROJ_DIR/.codebase/pipelines/compiler/build_and_lit_test.sh
+# build brt
 bash $ROOT_PROJ_DIR/.codebase/pipelines/runtime/build_and_test.sh --cuda --python --no-test
+# build torch-frontend
+bash $ROOT_PROJ_DIR/.codebase/pipelines/frontends/torch-frontend/build_with_prebuilt.sh
 
 mkdir $OUTPUT_DIR
 cp $ROOT_PROJ_DIR/compiler/build/python/dist/*.whl $OUTPUT_DIR/
 cp $ROOT_PROJ_DIR/runtime/build/python/_brt.so $OUTPUT_DIR/
 cp $ROOT_PROJ_DIR/runtime/build/lib/libbrt.so $OUTPUT_DIR/
+cp $ROOT_PROJ_DIR/frontends/torch-frontend/build/torch-frontend/python/dist/*.whl $OUTPUT_DIR/
 
 popd
