754eeeff92f481119ecab3103961da1005d58480
章玄润 <zhangxuanrun@bytedance.com>
2023-05-26 03:35:29 +0000

    [scm] Build torch-frontend in parallel 4

--- .codebase/pipelines/build_scm.sh
+++ .codebase/pipelines/build_scm.sh
@@ -16,7 +16,7 @@ bash $ROOT_PROJ_DIR/.codebase/pipelines/compiler/build_and_lit_test.sh
 # build brt
 bash $ROOT_PROJ_DIR/.codebase/pipelines/runtime/build_and_test.sh --cuda --python --no-test
 # build torch-frontend
-bash $ROOT_PROJ_DIR/.codebase/pipelines/frontends/torch-frontend/build_with_prebuilt.sh
+CMAKE_BUILD_PARALLEL_LEVEL=4 bash $ROOT_PROJ_DIR/.codebase/pipelines/frontends/torch-frontend/build_with_prebuilt.sh
 
 mkdir $OUTPUT_DIR
 cp $ROOT_PROJ_DIR/compiler/build/python/dist/*.whl $OUTPUT_DIR/
