6810cc0797d1b3bc2040477176d0fc292815270b
Yuanqiang Liu <liuyuanqiang.yqliu@bytedance.com>
2023-06-13 19:44:47 +0800

    [torch frontend] update torch-mlir

--- .codebase/pipelines/frontends/torch-frontend/test.sh
+++ .codebase/pipelines/frontends/torch-frontend/test.sh
@@ -11,7 +11,7 @@ export http_proxy='http://sys-proxy-rd-relay.byted.org:8118';
 export https_proxy='http://sys-proxy-rd-relay.byted.org:8118';
 export no_proxy='*.byted.org'
 
-python3 -m pip install --extra-index-url https://download.pytorch.org/whl/nightly/cu118 --pre torch==2.1.0.dev20230531+cu118
+python3 -m pip install --extra-index-url https://download.pytorch.org/whl/nightly/cu118 --pre torch==2.1.0.dev20230612+cu118
 
 pushd $PROJ_DIR
 PYTHONPATH=./build/python_packages/ python3 -m pytest torch-frontend/python/test
