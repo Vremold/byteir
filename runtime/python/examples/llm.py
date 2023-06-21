b8977e6d48f5efcc0d8589eb85ff6054e9473386
杨程旭 <yangchengxu@bytedance.com>
2023-06-21 17:22:45 +0000

    [runtime] add int8 Dtype
    * [runtime] add int8 Dtype
    * format
    * update
    
    https://code.byted.org/byteir/byteir/merge_requests/655

--- runtime/python/examples/llm.py
+++ runtime/python/examples/llm.py
@@ -9,25 +9,29 @@ import time
 LLM_MODEL_PATH = sys.argv[1]
 
 def ToTorchDType(dtype : _brt.DType):
-    if dtype == _brt.DType.float16:
-        return torch.float16
     if dtype == _brt.DType.float32:
         return torch.float32
-    if dtype == _brt.DType.float64:
-        return torch.float64
-    if dtype == _brt.DType.bool:
-        return torch.bool
-    if dtype == _brt.DType.uint8:
-        return torch.uint8
     if dtype == _brt.DType.int32:
         return torch.int32
     if dtype == _brt.DType.int64:
         return torch.int64
-    return "unsupporetd data type"
+    if dtype == _brt.DType.uint8:
+        return torch.uint8
+    if dtype == _brt.DType.float16:
+        return torch.float16
+    if dtype == _brt.DType.float64:
+        return torch.float64
+    if dtype == _brt.DType.bool:
+        return torch.bool
+    if dtype == _brt.DType.int8:
+        return torch.int8
+    if dtype == _brt.DType.int16:
+        return torch.int16
+    raise RuntimeError("unsupporetd data type: {}".format(int(dtype)))
 
 def main():
     session = _brt.Session(alloc_func=caching_allocator_alloc, free_func=caching_allocator_delete)
-    model_path = os.path.join(os.path.dirname(__file__), LLM_MODEL_PATH)
+    model_path = LLM_MODEL_PATH
     session.load(model_path)
     req = session.new_request_context(torch.cuda.current_stream()._as_parameter_.value)
 
