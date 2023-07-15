a055e2533d07bbb9a9ee1756eab662bf0c0be4dd
刘渊强 <liuyuanqiang.yqliu@bytedance.com>
2023-07-15 10:19:51 +0000

    [scm] fix build scm for revert [skipped]
    * [scm] fix build scm for revert
    * update
    
    https://code.byted.org/byteir/byteir/merge_requests/714

--- .codebase/pipelines/build_scm_for_revert.sh
+++ .codebase/pipelines/build_scm_for_revert.sh
@@ -3,6 +3,9 @@
 set -e
 set -x
 
+http_proxy=http://bj-rd-proxy.byted.org:3128 https_proxy=http://bj-rd-proxy.byted.org:3128 python3.7 -m pip install --no-cache-dir -f https://download.pytorch.org/whl/torch_stable.html torch==1.8.1+cu111
+
+
 CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # path to byteir root
 ROOT_PROJ_DIR="$CUR_DIR/../.."
@@ -12,9 +15,12 @@ pushd $ROOT_PROJ_DIR/frontends/torch-frontend/torch-frontend/lib/CustomOp
 rm -rf ./build
 mkdir build
 
+export CUDACXX=/usr/local/cuda/bin/nvcc
 pushd build
-cmake .. -DPython3_EXECUTABLE=python3
+cmake .. -DPython3_EXECUTABLE=python3.7
 make -j4
+
+mkdir $OUTPUT_DIR
 cp ./libcustom_op.so $OUTPUT_DIR/
 popd
 
