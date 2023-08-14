61422f4dbec5d5ecc2c4a35db0dea5f85f25a61e
刘渊强 <liuyuanqiang.yqliu@bytedance.com>
2023-08-14 08:35:10 +0000

    [ci] add send message to daily ci
    * [ci] add send message to daily ci
    * update
    * update
    
    https://code.byted.org/byteir/byteir/merge_requests/787

---
+++ .codebase/pipelines/scripts/send_feisu_msg.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import json
+import requests
+
+FEISHU_TOKENS = {
+    'byteir_ci_group': '568196b1-1ada-429e-9582-d8990f3cbd35',
+}
+
+
+def send_feishu_msg(title, msg, msgurl="", msg_type="post", cfg_key="byteir_ci_group", access_token=""):
+    """
+    :param title:
+    :param msg:
+    :param msgurl:
+    :param msg_type: text | post
+    :param cfg_key:
+    :param access_token:
+    :return:
+    """
+    tk = access_token if access_token else FEISHU_TOKENS.get(cfg_key)
+    webhook = f"https://open.feishu.cn/open-apis/bot/v2/hook/{tk}"
+    post_content = {
+        "zh_cn": {
+            "title": f"{title}",
+            "content": [
+                [{
+                    "tag": "text",
+                    "text": f"{msg} "
+                }, {
+                    "tag": "a",
+                    "text": f"{msgurl}",
+                    "href": f"{msgurl}"
+                }]
+            ]
+        }
+    }
+    payload = {
+        "msg_type": f"{msg_type}",
+        "content": {f"{msg_type}": post_content},
+    }
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url=webhook, headers=headers, data=json.dumps(payload))
+
+if __name__ == "__main__":
+    send_feishu_msg("ByteIR Daily CI", "byteir daily ci has completed")
