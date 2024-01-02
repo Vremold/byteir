edaf1e44b21c5873fee5ba4eb939d0bab7140ec8
Yan Xu <yan.xu0210@bytedance.com>
2024-01-02 07:36:51 +0000

    [onnx-frontend] Add lstm loop and more ops
    * [onnx-frontend] add LSTM lowering to mhlo.while
    * [onnx-frontend] restore lstm lit test
    * [onnx-frontend] add enableUnroll option
    * [onnx-frontend] replace onnx_opt_command with onnx_frontend_option
    * [onnx-frontend] fix lit test & add pass to pass.h
    * [onnx-frontend] add HardSigmoid
    * [onnx-frontend] add elu
    * [onnx-frontend] add DepthToSpace
    * [onnx-frontend] lower to stablehlo.dynamic_slice if possible
    * [onnx-frontend] update mhlo-tools and use Interpreter in with scope
    
    https://code.byted.org/byteir/byteir/merge_requests/1015

--- .codebase/pipelines/frontends/onnx-frontend/envsetup.sh
+++ .codebase/pipelines/frontends/onnx-frontend/envsetup.sh
@@ -31,7 +31,7 @@ function of_envsetup() {
   export no_proxy='*.byted.org'
 
   # install requirements
-  python3 -m pip install https://tosv.byted.org/obj/turing/byteir/mhlo_tools-1.1.0-cp39-cp39-linux_x86_64.whl
+  python3 -m pip install https://tosv.byted.org/obj/turing/byteir/mhlo_tools-1.2.4-cp39-cp39-linux_x86_64.whl
   python3 -m pip install -r $ONNX_FRONTEND_ROOT/requirements.txt
 
   # init submodule
